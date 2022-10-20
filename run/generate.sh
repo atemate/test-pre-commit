f20e2d014e148ed0d4c945cf8ec2daf97cbba52b
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 13:12:51 +0200

    I'm sorry.

--- run/generate.sh
+++ run/generate.sh
@@ -3,8 +3,8 @@ set -eux
 
 OUT_DIR=$(dirname ${BASH_SOURCE[0]})/out
 
-# rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
+rm -f $OUT_DIR/*
 for i in $(seq 1 3); do
     # echo "Writing $OUT_DIR/$i.txt"
     echo "text-$i" > $OUT_DIR/$i.txt
