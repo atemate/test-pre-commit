6d95c0f381256cd529858e7ab79a2e1e33ed886f
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 13:12:26 +0200

    SHIT ===> GOLD

--- run/generate.sh
+++ run/generate.sh
@@ -5,7 +5,7 @@ OUT_DIR=$(dirname ${BASH_SOURCE[0]})/out
 
 # rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
-for i in $(seq 1 5); do
+for i in $(seq 1 3); do
     # echo "Writing $OUT_DIR/$i.txt"
     echo "text-$i" > $OUT_DIR/$i.txt
 done
