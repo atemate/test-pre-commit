f5bed46b52fad31854051526c09459c02885b545
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 13:17:02 +0200

    Is there an achievement for this?

--- run/generate.sh
+++ run/generate.sh
@@ -4,10 +4,10 @@ set -eux
 OUT_DIR=$(dirname ${BASH_SOURCE[0]})/out
 NUM_FILES=${NUM_FILES:-5}
 
-mkdir -p $OUT_DIR
-rm -f $OUT_DIR/*
+rm -rf $OUT_DIR
+mkdir $OUT_DIR
 for i in $(seq 1 $NUM_FILES); do
     # echo "Writing $OUT_DIR/$i.txt"
-    echo "text-$i/$NUM_FILES" > $OUT_DIR/$i.txt
+    echo "text-$i" > $OUT_DIR/$i.txt
 done
 # git 
