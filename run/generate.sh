dbd978555a5fd5e7bb48267b3c04013f70eab910
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 13:14:32 +0200

    QuickFix.

--- run/generate.sh
+++ run/generate.sh
@@ -8,5 +8,6 @@ mkdir -p $OUT_DIR
 rm -f $OUT_DIR/*
 for i in $(seq 1 $NUM_FILES); do
     # echo "Writing $OUT_DIR/$i.txt"
-    echo "text-$i" > $OUT_DIR/$i.txt
+    echo "text-$i/$NUM_FILES" > $OUT_DIR/$i.txt
 done
+git 
