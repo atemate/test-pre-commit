47d28d647c5060dc19e329d2d1bae6b612d5eaea
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 13:09:38 +0200

    this is Spartaaaaaaaa

--- run/generate.sh
+++ run/generate.sh
@@ -5,7 +5,7 @@ OUT_DIR=$(dirname ${BASH_SOURCE[0]})/out
 
 rm -rf $OUT_DIR
 mkdir $OUT_DIR
-for i in $(seq 1 3); do
+for i in $(seq 1 5); do
     echo "Writing $OUT_DIR/$i.txt"
     echo "text-$i" > $OUT_DIR/$i.txt
 done
