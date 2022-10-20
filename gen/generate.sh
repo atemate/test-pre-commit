87bd70b4db9d1d6e36dc521b2c47decaf77d5379
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2022-10-20 12:35:17 +0200

    v0-1

--- gen/generate.sh
+++ gen/generate.sh
@@ -5,7 +5,7 @@ OUT_DIR=$(dirname ${BASH_SOURCE[0]})/generated
 
 rm -rf $OUT_DIR
 mkdir $OUT_DIR
-for i in $(seq 1 10); do
+for i in $(seq 1 3); do
     echo "Writing $OUT_DIR/$i.txt"
     echo "text-$i" > $OUT_DIR/$i.txt
 done
